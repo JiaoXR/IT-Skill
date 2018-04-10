b57f1f5c9bd648c5fc783b95e7aaf4b7e98e70ad
jiaoxiangru <jiaoxiangru@adbaitai.com>
2018-04-10 23:43:26 +0800

    add pic

---
+++ Design-Patterns/src/com/jaxer/example/prototype/Circle.java
@@ -0,0 +1,8 @@
+package com.jaxer.example.prototype;
+
+/**
+ * @author jaxer
+ * date 10/04/2018
+ */
+public class Circle {
+}
---
+++ Design-Patterns/src/com/jaxer/example/prototype/TestPrototype.java
@@ -0,0 +1,10 @@
+package com.jaxer.example.prototype;
+
+/**
+ * 测试类
+ *
+ * @author jaxer
+ * date 10/04/2018
+ */
+public class TestPrototype {
+}
